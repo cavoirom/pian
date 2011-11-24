90632555b177b96336a993eec00ffaf71a1df1fb
cavoirom <cavoirom.wanted@yahoo.com>
2011-11-24 10:18:48 +0700

    add player to Homeage

--- src/pian/client/Pian.java
+++ src/pian/client/Pian.java
@@ -5,13 +5,21 @@ import java.util.Iterator;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.Style.Overflow;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.AttachEvent;
+import com.google.gwt.event.logical.shared.AttachEvent.Handler;
+import com.google.gwt.event.shared.GwtEvent.Type;
+import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.DecoratorPanel;
 import com.google.gwt.user.client.ui.DockPanel;
+import com.google.gwt.user.client.ui.FocusWidget;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Panel;
@@ -31,7 +39,6 @@ public class Pian implements EntryPoint {
 	private CheckBox titleCheckBox = new CheckBox("Bài hát");
 	private CheckBox artistCheckBox = new CheckBox("Ca sĩ");
 	private CheckBox albumCheckBox = new CheckBox("Album");
-	private Image backgroundImage = new Image();
 	private Image logoImage = new Image();
 	private DockPanel searchPanel = new DockPanel();
 	private HorizontalPanel controlPanel = new HorizontalPanel();
@@ -78,10 +85,9 @@ public class Pian implements EntryPoint {
 		/* render full search box. */
 		
 		/* Test result panel */
-		playerPanel.add(new HTML("This is Player Panel"));
 		allResultPanel.setSpacing(5);
-		allResultPanel.add(renderCellPanel("id001", "Làm Ơn", "Trần Trung Đức", "ab001", "Đang cập nhật"));
-		allResultPanel.add(renderCellPanel("id001", "Vài Lần Đón Đưa", "Lê Hiếu", "ab001", "Đang cập nhật"));
+		allResultPanel.add(renderCellPanel("http://stream3.mp3.zdn.vn/ES3stAYFQh/452b03c7ccc92e8bc7b50838207651e2/4eccceb0/2011/11/22/5/2/52e7b9a0ee8627037ca3a2a8235012fe.mp3", "Làm Ơn", "Trần Trung Đức", "ab001", "Đang cập nhật"));
+		allResultPanel.add(renderCellPanel("http://stream.mp3.zdn.vn/fsfsdfdsfdserwrwq3/4bf90c3cf20967f828e3d33e2e774d74/4eccd9c0/d/cc/dcc132fbbc40e0d1c12c28523249bb91.mp3", "Vài Lần Đón Đưa", "Lê Hiếu", "ab001", "Đang cập nhật"));
 		/* Test result panel */
 		
 		/* render result box. */
@@ -92,7 +98,28 @@ public class Pian implements EntryPoint {
 		songPanel.add(albumResultPanel, "Album");
 		songPanel.selectTab(0);
 		
+		
+		
 		playerPanel.setPixelSize(280, 30);
+		playerPanel.add(new HTML("<div id=\"mainPlayer\">Loading the player ...</div>"));
+		playerPanel.addAttachHandler(new Handler() {
+			@Override
+			public void onAttachOrDetach(AttachEvent event) {
+				if (event.isAttached())
+					Pian.renderPlayer();
+				
+			}
+		});
+		/*playerPanel.add(new HTML("<embed "+
+				"flashvars=\"file=http://media.vinahoo.com/secdl/1752f958c3cc8fdd1798dbc19758a530/4eccb263/bbmp3/music/2010/10/11/GBC1Q9.mp3\" " +
+				"allowfullscreen=\"false\" " +
+				"allowscripaccess=\"always\" " +
+				"id=\"player1\" " +
+				"name=\"player1\" " +
+				"src=\"player/player.swf\" " +
+				"width=\"270\" " +
+				"height=\"24\" " +
+				"/>"));*/
 		
 		playlistPanel.setWidth("280px");
 		playlistPanel.add(playlistLabel);
@@ -104,7 +131,6 @@ public class Pian implements EntryPoint {
 		resultPanel.add(songPanel, DockPanel.WEST);
 		resultPanel.add(playerPanel, DockPanel.NORTH);
 		resultPanel.add(playlistPanel, DockPanel.NORTH);
-		/* render result box. */
 		
 		// Associate the searchPanel, resultPanel with the pageContainer.
 		pageContainer.setBorderWidth(0);
@@ -121,13 +147,19 @@ public class Pian implements EntryPoint {
 		searchBox.setFocus(true);
 	}
 
-	private DockPanel renderCellPanel(String id, String songTitle, String artist, String albumId, String album) {
-		Label titleAndArtistLabel = new Label(songTitle + " - " + artist);
-		Label albumNameLabel = new Label("Album: " + album);
+	private DockPanel renderCellPanel(final String id, String songTitle, String artist, String albumId, String album) {
+		Hyperlink titleLink = new Hyperlink(songTitle, id);
+		titleLink.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				Pian.playSong(id);
+			}
+		});
+		Hyperlink albumNameLabel = new Hyperlink(album, albumId);
 		
 		VerticalPanel songInfoPanel = new VerticalPanel();
 		songInfoPanel.setWidth("500px");
-		songInfoPanel.add(titleAndArtistLabel);
+		songInfoPanel.add(titleLink);
 		songInfoPanel.add(albumNameLabel);
 		
 		Label addToPlaylistLabel = new Label("Thêm vào Playlist");
@@ -147,4 +179,19 @@ public class Pian implements EntryPoint {
 		return cellPanel;
 	}
 	
+	public static native void renderPlayer() /*-{
+		$wnd.jwplayer("mainPlayer").setup({
+			flashplayer: "/player/player.swf",
+			file: "http://stream3.mp3.zdn.vn/ES3stAYFQh/452b03c7ccc92e8bc7b50838207651e2/4eccceb0/2011/11/22/5/2/52e7b9a0ee8627037ca3a2a8235012fe.mp3",
+			controlbar: "bottom",
+			width: 270,
+			height: 24,
+		});
+	}-*/;
+	
+	public static native void playSong(String url) /*-{
+		//$wnd.jwplayer("mainPlayer").load(url);
+		$wnd.jwplayer("mainPlayer").play();
+		//alert(url);
+	}-*/;
 }
