72feba52d016b926258eb36614fcc680dba982fd
cavoirom <cavoirom.wanted@yahoo.com>
2011-12-08 14:45:41 +0700

    change homepage: hide resultPanel by default

--- src/pian/client/Pian.java
+++ src/pian/client/Pian.java
@@ -1,18 +1,19 @@
 package pian.client;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.logical.shared.AttachEvent;
 import com.google.gwt.event.logical.shared.AttachEvent.Handler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.DockPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -22,48 +23,53 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class Pian implements EntryPoint {
 	private VerticalPanel pageContainer = new VerticalPanel();
-	
-	private SuggestBox searchBox = new SuggestBox();
-	private Button submitButton = new Button("Tìm nhạc");
-	private CheckBox allOptionCheckBox = new CheckBox("Tất cả");
-	private CheckBox titleCheckBox = new CheckBox("Bài hát");
-	private CheckBox artistCheckBox = new CheckBox("Ca sĩ");
-	private CheckBox albumCheckBox = new CheckBox("Album");
-	private Image logoImage = new Image();
 	private DockPanel searchPanel = new DockPanel();
-	private HorizontalPanel commandPanel = new HorizontalPanel();
-	private DockPanel optionPanel = new DockPanel();
-	
 	private DockPanel resultPanel = new DockPanel();
-	private TabPanel songPanel = new TabPanel();
-	private VerticalPanel allResultPanel = new VerticalPanel();
-	private VerticalPanel songResultPanel = new VerticalPanel();
-	private VerticalPanel artistResultPanel = new VerticalPanel();
-	private VerticalPanel albumResultPanel = new VerticalPanel();
-	private VerticalPanel playerPanel = new VerticalPanel();
-	private VerticalPanel playlistPanel = new VerticalPanel();
-	private Label playlistLabel = new Label("Playlist của bạn");
+	private HorizontalPanel footerPanel = new HorizontalPanel();
 	
 	@Override
 	public void onModuleLoad() {
 		/* Begin - SEACH BOX */
 		// logo in the left of search box.
+		Image logoImage = new Image();
 		logoImage.setUrl("images/logo.jpg");
 		logoImage.setSize("115px", "110px");
 		
 		// input keyword here
+		SuggestBox searchBox = new SuggestBox();
 		searchBox.setWidth("300px");
 		
 		// start search on click
+		Button submitButton = new Button("Tìm nhạc");
 		submitButton.getElement().getStyle().setPaddingLeft(10, Unit.PX);
 		
+		submitButton.addClickHandler(new ClickHandler(){
+
+			@Override
+			public void onClick(ClickEvent event) {
+				searchPanel.removeStyleName("new");
+				resultPanel.setVisible(true);
+			}
+			
+		});
+		
 		// add searchBox and submitButton to its container panel.
+		HorizontalPanel commandPanel = new HorizontalPanel();
 		commandPanel.setBorderWidth(0);
 		commandPanel.setWidth("400px");
 		commandPanel.add(searchBox);
 		commandPanel.add(submitButton);
 			
-		// search options here.
+		// option for search all field (Song's title, Artist, Album)
+		CheckBox allOptionCheckBox = new CheckBox("Tất cả");
+		// search Song title only
+		CheckBox titleCheckBox = new CheckBox("Bài hát");
+		// search Artist only
+		CheckBox artistCheckBox = new CheckBox("Ca sĩ");
+		// search Album only
+		CheckBox albumCheckBox = new CheckBox("Album");
+		// add options into its container panel, also commandPanel.
+		DockPanel optionPanel = new DockPanel();
 		optionPanel.add(commandPanel, DockPanel.NORTH);
 		optionPanel.add(allOptionCheckBox, DockPanel.WEST);
 		optionPanel.add(titleCheckBox, DockPanel.WEST);
@@ -74,16 +80,23 @@ public class Pian implements EntryPoint {
 		searchPanel.setVerticalAlignment(VerticalPanel.ALIGN_MIDDLE);
 		searchPanel.add(logoImage, DockPanel.WEST);
 		searchPanel.add(optionPanel, DockPanel.EAST);
+		
+		// margin to the middle of page
+		searchPanel.addStyleName("new");
 		/* End - SEACH BOX */
 		
-		/* Test result panel */
-		allResultPanel.setSpacing(5);
-		allResultPanel.add(renderCellPanel("http://stream3.mp3.zdn.vn/ES3stAYFQh/452b03c7ccc92e8bc7b50838207651e2/4eccceb0/2011/11/22/5/2/52e7b9a0ee8627037ca3a2a8235012fe.mp3", "Làm Ơn", "Trần Trung Đức", "ab001", "Đang cập nhật"));
-		allResultPanel.add(renderCellPanel("http://stream.mp3.zdn.vn/fsfsdfdsfdserwrwq3/4bf90c3cf20967f828e3d33e2e774d74/4eccd9c0/d/cc/dcc132fbbc40e0d1c12c28523249bb91.mp3", "Vài Lần Đón Đưa", "Lê Hiếu", "ab001", "Đang cập nhật"));
-		/* Test result panel */
+		/* Begin - SONG PANEL, This panel display the list of songs match the search keyword */
+		// this panel display all result matched.
+		VerticalPanel allResultPanel = new VerticalPanel();
+		// this panel display song matched.
+		VerticalPanel songResultPanel = new VerticalPanel();
+		// this panel display artist matched.
+		VerticalPanel artistResultPanel = new VerticalPanel();
+		// this panel display album matched.
+		VerticalPanel albumResultPanel = new VerticalPanel();
 		
-		/* Begin - SONG PANEL */
-		// This panel display the list of songs match the search keyword
+		// add above panels into container tab
+		TabPanel songPanel = new TabPanel();
 		songPanel.setWidth("700px");
 		songPanel.add(allResultPanel, "Tất cả");
 		songPanel.add(songResultPanel, "Tựa đề");
@@ -93,6 +106,7 @@ public class Pian implements EntryPoint {
 		/* End - SONG PANEL */
 		
 		/* Begin - PLAYER PANEL */
+		VerticalPanel playerPanel = new VerticalPanel();
 		playerPanel.setPixelSize(280, 30);
 		playerPanel.add(new HTML("<div id=\"mainPlayer\">Loading the player ...</div>"));
 		playerPanel.addAttachHandler(new Handler() {
@@ -106,10 +120,14 @@ public class Pian implements EntryPoint {
 		/* End - PLAYER PANEL */
 		
 		/* Begin - PLAYLIST PANEL */
+		Label playlistLabel = new Label("Playlist của bạn");
+		
+		VerticalPanel playlistPanel = new VerticalPanel();
 		playlistPanel.setWidth("280px");
 		playlistPanel.add(playlistLabel);
 		/* End - PLAYLIST PANEL */
 		
+		/* Begin - RESULT PANEL */
 		// Associate the songPanel, playerPanel, playlistPanel with the resultPanel.
 		resultPanel.setWidth("980px");
 		resultPanel.setHeight("100%");
@@ -118,13 +136,24 @@ public class Pian implements EntryPoint {
 		resultPanel.add(playerPanel, DockPanel.NORTH);
 		resultPanel.add(playlistPanel, DockPanel.NORTH);
 		
-		// Associate the searchPanel, resultPanel with the pageContainer.
+		// hide resultPanel by default
+		resultPanel.setVisible(false);
+		/* End - RESULT PANEL */
+		
+		/* Begin - FOOTER PANEL */
+		
+		footerPanel.setHorizontalAlignment(HorizontalPanel.ALIGN_CENTER);
+		footerPanel.add(new HTML("&copy;2011 by Hữu Phong, Đỗ Tâm, cavoirom"));
+		/* End - FOOTER PANEL */
+		
+		// Associate the searchPanel, resultPanel, footerPanel with the pageContainer.
 		pageContainer.setBorderWidth(0);
 		pageContainer.setWidth("100%");
 		pageContainer.setHorizontalAlignment(HorizontalPanel.ALIGN_CENTER);
 		pageContainer.setSpacing(10);
 		pageContainer.add(searchPanel);
 		pageContainer.add(resultPanel);
+		pageContainer.add(footerPanel);
 		
 		// Associate the pageContainer with the RootPanel to display it.
 		RootPanel.get("pageContainer").add(pageContainer);
@@ -133,37 +162,13 @@ public class Pian implements EntryPoint {
 		searchBox.setFocus(true);
 	}
 
-	private DockPanel renderCellPanel(final String id, String songTitle, String artist, String albumId, String album) {
-		Hyperlink titleLink = new Hyperlink(songTitle, id);
-		titleLink.addClickHandler(new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				Pian.playSong(id);
-			}
-		});
-		Hyperlink albumNameLabel = new Hyperlink(album, albumId);
-		
-		VerticalPanel songInfoPanel = new VerticalPanel();
-		songInfoPanel.setWidth("500px");
-		songInfoPanel.add(titleLink);
-		songInfoPanel.add(albumNameLabel);
-		
-		Label addToPlaylistLabel = new Label("Thêm vào Playlist");
-		Label downloadLabel = new Label("Tải về");
-		
-		VerticalPanel songOperationPanel = new VerticalPanel();
-		songOperationPanel.setHorizontalAlignment(HorizontalPanel.ALIGN_RIGHT);
-		songOperationPanel.setWidth("150px");
-		songOperationPanel.add(addToPlaylistLabel);
-		songOperationPanel.add(downloadLabel);
-		
+	/* This code isn't work!
+	private DockPanel getSongCell(Song song) {
 		DockPanel cellPanel = new DockPanel();
-		cellPanel.setWidth("100%");
-		cellPanel.add(songInfoPanel, DockPanel.WEST);
-		cellPanel.add(songOperationPanel, DockPanel.EAST);
-		
+
 		return cellPanel;
 	}
+	*/
 	
 	private static native void renderPlayer() /*-{
 		$wnd.jwplayer("mainPlayer").setup({
