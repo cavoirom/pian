d4361a9d9a5877d43315bb0dfe14ffff2bff76d6
huuphong <visaobang892007@yahoo.com>
2011-12-09 16:31:31 +0700

    Fix path sqlite

--- src/pian/model/dao/Test.java
+++ src/pian/model/dao/Test.java
@@ -1,15 +1,12 @@
 package pian.model.dao;
 
-import pian.model.Album;
-import pian.model.Artist;
-import pian.model.Song;
 
 public class Test {
 	public static void main(String[] args) {
-		Artist artist = new Artist("Lam Trường");
-		Album album =  new Album("album1");
-		album.setArtist(artist);
-		Song song = new Song("Title 1", artist, "Cộng hòa xã hội chủ nghĩa", album);
+//		Artist artist = new Artist("Lam Trường");
+//		Album album =  new Album("album1");
+//		album.setArtist(artist);
+//		Song song = new Song("Title 1", artist, "Cộng hòa xã hội chủ nghĩa", album);
 //		System.out.println(new SongDAOImpl().storeSong(song));
 		System.out.println(new SongDAOImpl().loadSong(12).getLink());
 		System.out.println(new SongDAOImpl().loadSong(12).getAlbum().getName());
